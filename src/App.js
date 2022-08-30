f31111ea93503c4ea26fc56d1b51bb7b84927172
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2022-08-30 18:40:02 -0400

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import GlobalStyle from './styles/GlobalStyle'
+import { Switch, Route, Routes } from 'react-router-dom';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <GlobalStyle />
+      <Nav /> 
+      <Routes>  
+         
+      </Routes>
     </div>
   );
 }
